306eefc937b9cdb67dcf701aa0a12330f128624a
AnaPanamito <ana.panamito@unl.edu.ec>
2025-02-19 08:07:29 -0500

    Update settings.py

--- SistemaFarmacia/settings.py
+++ SistemaFarmacia/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'SistemaFarmacia.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'SistemaFarmacia',
+        'USER': 'root',
+        'PASSWORD': 'AnaP123@',
+        'HOST': 'localhost',
+        'PORT': '3306',
+
 
     }
 }
